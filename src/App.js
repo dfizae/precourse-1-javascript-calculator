29609e181737f9028335787327ea92fd7e4f140d
Jaeyoung Kim <jaeyoungkim@Jaeyoungui-MacBookPro.local>
2025-10-17 01:52:01 +0900

    feat: Console 출력에 결과 값 표시 추가

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    
+    const input = await MissionUtils.Console.readLineAsync();
+    const result = this.calculate(input);
+    MissionUtils.Console.print(`결과 : ${result}`);
+  }
+
+  calculate(){
+
   }
 }
 
