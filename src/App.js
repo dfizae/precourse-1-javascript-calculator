0dc2a9b05ce39ccbfe25bfcdb81cf965de97b2cb
Jaeyoung Kim <jaeyoungkim@Jaeyoungui-MacBookPro.local>
2025-10-17 14:06:13 +0900

    fix: Console 입력 값에 사용자가 직접 입력할 수 있도록 변경

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    const input = await MissionUtils.Console.readLineAsync();
+    const input = await MissionUtils.Console.readLineAsync("덧셈할 문자열을 입력해주세요.\n");
     const result = this.calculate(input);
     MissionUtils.Console.print(`결과 : ${result}`);
   }
